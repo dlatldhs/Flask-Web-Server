c35ae13a804ac78b0f2e02667ff3792fb1c93491
dlatldhs <sionzz1713@gmail.com>
2021-10-07 20:35:33 +0900

    게시글 만들 때 오류 경고 메세지 출력

--- myproject/pybo/forms.py
+++ myproject/pybo/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 class QuestionForm(FlaskForm):
-    subject = StringField('제목',validators=[DataRequired()]) #글자수 제한있음
-    content = TextAreaField('내용',validators=[DataRequired()]) #글자수 제한없음
+    subject = StringField('제목',validators=[DataRequired('제목을 입력해주세용')]) #글자수 제한있음
+    content = TextAreaField('내용',validators=[DataRequired('내용을 입력해주세요ㅠㅠ')]) #글자수 제한없음
     # "제목" 은 폼 라벨임 이걸 이용해서 라벨 출력 가능함 validators 검증을 위해 사용되는 도구
     # DataRequired Eamil Lengh 등이 있음 
